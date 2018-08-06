cc2a1ede8fae7bf7b4e23042e212b8198eadfc35
Jason Pitts <juniorjr083@gmail.com>
2018-08-06 13:23:17 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -4,16 +4,16 @@ class App < Sinatra::Base
   
   
   get '/name' do 
-    puts "My nam is #{junior}"
+    puts "My nam is #_ _"
   
   end 
   
   get '/hometown' do 
-    puts "My hometown is #{NY}"
+    puts "My hometown is__"
   end 
   
   get '/favorite-song' do
-    puts "My favorite-song is #{space jams}"
+    puts "My favorite-song is__"
   
   end 
 
