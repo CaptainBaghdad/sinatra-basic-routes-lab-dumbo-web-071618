17bff83a483a1afac9e5e20d788661c628b4416d
Jason Pitts <juniorjr083@gmail.com>
2018-08-06 13:22:23 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  
+  get '/name' do 
+    puts "My nam is #{junior}"
+  
+  end 
+  
+  get '/hometown' do 
+    puts "My hometown is #{NY}"
+  end 
+  
+  get '/favorite-song' do
+    puts "My favorite-song is #{space jams}"
+  
+  end 
+
+
 end
